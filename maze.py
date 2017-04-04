782836c8efdd6c7554ea81380490c589060c10e3
vegar.andreas@gmail.com <vegar.andreas@gmail.com>
2017-04-04 19:33:42 +0200

    Testing out PIL Image

---
+++ maze.py
@@ -0,0 +1,18 @@
+
+# MAZE-object representing the nodes in a graph,
+# representing the paths in the maze
+class Maze:
+    class Node:
+        def __init__(self, position):
+            self.position = position #position = (y,x)
+            self.neighbours = [None, None, None, None] # left, top, right, bot
+
+    def __init__(self, im):
+
+        width, height = im.size
+        data = list(im.getdata()) #list of pixels, 0 = black, 1 = white
+        x = createMaze(self, width, height, data)
+
+    def createMaze(self, width, height, data):
+        print("hello")
+        return 0
---
+++ solve.py
@@ -0,0 +1,24 @@
+# Main-file
+# Solves the maze
+from PIL import Image
+import time
+from maze import Maze
+
+def solve(input_file):
+
+    im = loadImage(input_file)
+
+
+def loadImage(input_file):
+    print ("Loading image ", input_file)
+    im = Image.open(input_file)
+    return im
+
+
+def main():
+    print("Hello World")
+    input_file = input("File: ")
+    solve(input_file)
+    return 0
+
+main()
