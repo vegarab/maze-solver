eb56d544c1ec6f5249bdc80dbf4d53b8c429d6c0
vegar.andreas@gmail.com <vegar.andreas@gmail.com>
2017-04-05 17:39:53 +0200

    Added buildMatrix functionality with numpy

--- maze.py
+++ maze.py
@@ -1,6 +1,7 @@
 
 # MAZE-object representing the nodes in a graph,
 # representing the paths in the maze
+import numpy as np
 class Maze:
     class Node:
         def __init__(self, position):
@@ -8,11 +9,14 @@ class Maze:
             self.neighbours = [None, None, None, None] # left, top, right, bot
 
     def __init__(self, im):
-
         width, height = im.size
         data = list(im.getdata()) #list of pixels, 0 = black, 1 = white
-        x = self.buildMaze(width, height, data)
+    #    data = self.buildMatrix(width, height, data)
+    #    x = self.buildMaze(width, height, data)
 
     def buildMaze(self, width, height, data):
-        print "hello"
         return 0
+    # convert 1d list to 2d-list
+    def buildMatrix(self, width, height, data):
+        newList = np.array(data).reshape(width, height)
+        return newList
--- solve.py
+++ solve.py
@@ -5,15 +5,13 @@ import time
 from maze import Maze
 
 def solve(input_file):
-
     im = loadImage(input_file)
-
+    data = list(im.getdata())
 
 def loadImage(input_file):
     print ("Loading image ", input_file)
     im = Image.open(input_file)
     return im
-
 
 def main():
     print("Hello World")
