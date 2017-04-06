3f6447b96fec14cf02b2e9f9f2695be0b443ca89
vegar.andreas@gmail.com <vegar.andreas@gmail.com>
2017-04-06 19:06:28 +0200

    buildMaze is OK! Createst correct nodes

--- maze.py
+++ maze.py
@@ -34,6 +34,8 @@ class Maze:
 
     def buildMaze(self, width, height, data):
 
+        print(data)
+
         # first and end-node
         self.start = None
         self.end = None
@@ -42,30 +44,28 @@ class Maze:
         self.nodeCount = 0
 
         # nodes above current row
-        topNodes = [] * width
+        topNodes = [None] * width # +1 bcs for's are !0-indexed
+        topNodes[width-1] = 1
 
         # find start node
         for i in range(1, width-1):
             if data[0][i] == 1:
                 self.start = Maze.Node((0, i))
-                nodeCount += 1
+                self.nodeCount += 1
+                print(self.start.position)
                 topNodes[i] = self.start # add start node to topNodes at pos
                 break
 
-        # print out position of start and end node
-        print self.start.position
-        print self.end.position
-
-        for y in range(1, height-2): # 1 and -2 to exclude last and first row
+        for y in range(1, height-1): # 1 and -2 to exclude last and first row
             # new node
             n = None
             # node left of current
             left = None
 
             for x in range(1, width-1): # 1 to exclude left wall and NullPointer
-
+                print (x)
                 # if on wall, set topNode to none, then cont
-                if data[y][x] > 0:
+                if data[y][x] < 1:
                     topNodes[x] = None
                     continue
 
@@ -73,31 +73,36 @@ class Maze:
                 if data[y][x-1] > 0 and data[y][x+1] > 0:
                     # create node if path above or below
                     if data[y-1][x] > 0 or data[y+1][x] > 0:
-                        n.Maze.Node((y, x))
-                        nodeCount += 1
+                        print("PATH PATH PATH ", x, y)
+                        n = Maze.Node((y, x))
+                        self.nodeCount += 1
                         left.addNeighbour(1, n)
                         left = n
 
                 # WALL PATH PATH (start of corridor)
                 if data[y][x-1] < 1 and data[y][x+1] > 0:
                     # create node
-                    n.Maze.Node((y, x))
-                    nodeCount += 1
+                    print("PATH PATH PATH ", x, y)
+                    n = Maze.Node((y, x))
+                    self.nodeCount += 1
                     left = n
 
                 # PATH PATH WALL (end of corridor)
                 if data[y][x-1] > 0 and data[y][x+1] < 1:
                     # create node
-                    n.Maze.Node((y, x))
-                    nodeCount += 1
+                    print("PATH PATH WALL ", x, y)
+                    n = Maze.Node((y, x))
+                    self.nodeCount += 1
                     left.addNeighbour(1, n)
                     left = n
 
                 # WALL PATH WALL (dead end)
                 if data[y][x-1] < 1 and data[y][x+1] < 1:
-                    # create node
-                    n.Maze.Node((y, x))
-                    nodeCount += 1
+                    # create node if nothing below or above
+                    if data[y+1][x] < 1 or data[y-1][x] < 1:
+                        print("WALL PATH WALL ", x, y)
+                        n = Maze.Node((y, x))
+                        self.nodeCount += 1
 
                 # check topNodes
                 if n != None:
@@ -105,16 +110,13 @@ class Maze:
                     if data[y-1][x] > 0:
                         # connect topNode to n, make n a topNode
                         topNodes[x].addNeighbour(3, n)
-                        topNodes[x] = n
-                # else remove set crnt topNode to None for next iteration
-                else:
-                    topNodes[x] = None
+                    topNodes[x] = n
 
         # find end node
         for i in range(1, width-1):
             if data[height-1][i] == 1:
                 self.end = Maze.Node((height-1, i))
-                nodeCount += 1
+                self.nodeCount += 1
                 # connects end node to the node above
                 topNodes[i].addNeighbour(3, self.end)
 
--- solve.py
+++ solve.py
@@ -8,7 +8,7 @@ def solve(input_file):
     print ("Loading image ", input_file)
     im = loadImage(input_file)
 
-    print "Building maze"
+    print ("Building maze")
     t0 = time.time()
     maze = Maze(im)
     t1 = time.time()
@@ -20,7 +20,6 @@ def loadImage(input_file):
     return Image.open(input_file)
 
 def main():
-    print "Hello World"
     input_file = input("File: ")
     solve("/home/vegarab/python/maze-solver/mazes/"+input_file)
     return 0
