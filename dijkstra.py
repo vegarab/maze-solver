87b245c4cfed837c2d2b35c55b5844f78a6b78d5
vegar.andreas@gmail.com <vegar.andreas@gmail.com>
2017-04-07 10:15:43 +0200

    Added TODO for dijkstra implementation

---
+++ dijkstra.py
@@ -0,0 +1,8 @@
+"""
+TODO:
+- Look closely at dijkstra's algorithm and different implementations
+- Learn Heaps
+- Learn FibonacciHeap
+- Create data-structure to hold the paths
+- Creat data-structure for the queue
+"""
